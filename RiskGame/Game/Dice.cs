8eb4266f689e5bac273e9b2344c1b383918a8a34
Joseph-Garvey <67016104+Joseph-Garvey@users.noreply.github.com>
2020-07-10 14:25:30 +0100

    Beggining of Risk Classic mode

---
+++ RiskGame/Game/Dice.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.ComponentModel;
+using System.Threading.Tasks;
+
+
+namespace RiskGame.Game
+{
+    public class Dice
+    {
+        private BackgroundWorker workerthread = new BackgroundWorker()
+        {
+            WorkerReportsProgress = true,
+            WorkerSupportsCancellation = true
+        };
+        public static List<Uri> sources = new List<Uri>
+        {
+            new Uri("pack://siteoforigin:,,,/Images/Dice/dice_1.png"),
+            new Uri("pack://siteoforigin:,,,/Images/Dice/dice_2.png"),
+            new Uri("pack://siteoforigin:,,,/Images/Dice/dice_3.png"),
+            new Uri("pack://siteoforigin:,,,/Images/Dice/dice_4.png"),
+            new Uri("pack://siteoforigin:,,,/Images/Dice/dice_5.png"),
+            new Uri("pack://siteoforigin:,,,/Images/Dice/dice_6.png"),
+        };
+        public int current;
+        Random rng = new Random();
+
+        public Dice()
+        {
+            workerthread.DoWork += Worker_DoWork;
+            workerthread.ProgressChanged += Worker_ProgressChanged;
+            workerthread.RunWorkerCompleted += Worker_RunWorkerCompleted;
+            current = 6;
+        }
+        private void StartRoll()
+        {
+            workerthread.RunWorkerAsync();
+        }
+        private void Worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                if (workerthread.CancellationPending == true)
+                {
+                    e.Cancel = true;
+                    return;
+                }
+                else
+                {
+                    int progressPercentage = Convert.ToInt32(((double)i / 6) * 100);
+                    current = rng.Next(0, 5);
+                    (sender as BackgroundWorker).ReportProgress(progressPercentage);
+                    Thread.Sleep(250);
+                }
+            }
+        }
+        void Worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+
+        }
+        void Worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            // on completed
+        }
+    }
+}
